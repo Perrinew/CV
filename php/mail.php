96210a98b137675def3c01456c24033758498c7a
Perrinew <106061214+Perrinew@users.noreply.github.com>
2022-07-06 15:18:43 +0200

    1.3

--- php/mail.php
+++ php/mail.php
@@ -1,5 +1,5 @@
 <?php
-    $retour = mail('perrinew62@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'from https://porte-folio-perrine-wengel.000webhostapp.com/html/cv_contact.html?');
+    $mail('perrinew62@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], ' ');
     if ($retour)
         echo '<p>Votre message a bien été envoyé.</p>';
     ?>
